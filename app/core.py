050febebce4ac980395d467cd85f59a63a7da38f
Marcos Vinicius Thibes Kemer <mircothibes@gmail.com>
2025-09-04 16:35:45 +0200

    core.py

--- app/core.py
+++ app/core.py
@@ -6,18 +6,26 @@ def download_video(url: str, path: str, audio_only: bool = False, progress_cb: O
         if progress_cb:
             progress_cb(d)
 
-    options =options = {
-    "outtmpl": f"{path}/%(title)s.%(ext)s",
-    "format": "bv*+ba/b",            
-    "merge_output_format": "mp4",    
-}
-    if audio_only:
-        options["format"] = "bestaudio/best"
-        options["postprocessors"] = [
-            {"key": "FFmpegExtractAudio", "preferredcodec": "mp3", "preferredquality": "192"}
-        ]
-    else:
-        options["format"] = "bv*+ba/b"
+    ydl_opts = {
+        "outtmpl": f"{path}/%(title)s.%(ext)s",
+        "progress_hooks": [_hook],
+    }
 
-    with YoutubeDL(options) as ydl:
+    if audio_only:
+        ydl_opts.update({
+            "format": "bestaudio/best",
+            "postprocessors": [{
+                "key": "FFmpegExtractAudio",
+                "preferredcodec": "mp3",
+                "preferredquality": "192",
+            }],
+        })
+    else:
+        # Melhor vídeo + áudio, cai para melhor único se necessário
+        ydl_opts.update({
+            "format": "bv*+ba/b",
+            "merge_output_format": "mp4",
+        })
+
+    with YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
